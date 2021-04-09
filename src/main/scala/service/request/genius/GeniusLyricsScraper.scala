1b4470fe55ff0e005d857ae996d1e7954cd4fc0f
davyera <davy.era@gmail.com>
2021-04-08 21:25:28 -0700

    Add functionality to grab multiple song lyrics from a sequence of GeniusSong in a future format

--- src/main/scala/service/request/genius/GeniusLyricsScraper.scala
+++ src/main/scala/service/request/genius/GeniusLyricsScraper.scala
@@ -1,11 +1,23 @@
 package service.request.genius
 
 import com.typesafe.scalalogging.StrictLogging
+import models.api.response.GeniusSong
 import org.jsoup.Jsoup
 
-class GeniusLyricsScraper extends StrictLogging {
+import scala.concurrent.{ExecutionContext, Future}
 
-  def scrapeLyrics(url: String): String = {
+class GeniusLyricsScraper(implicit val context: ExecutionContext) extends StrictLogging {
+
+  /** Scrapes Genius for lyrics for given songs
+   *  @return Seq of tuples: ([[GeniusSong]], Future of its lyrics)
+   */
+  def scrapeLyrics(songs: Seq[GeniusSong]): Seq[(GeniusSong, Future[String])] = {
+    songs.map { song: GeniusSong =>
+      song -> Future(scrapeLyrics(song.url))
+    }
+  }
+
+  protected[genius] def scrapeLyrics(url: String): String = {
     val fullDocument = Jsoup.connect(url).get()
     val rawLyrics = fullDocument.select(".lyrics").text()
     val cleanedLyrics = cleanLyrics(rawLyrics)
@@ -18,5 +30,4 @@ class GeniusLyricsScraper extends StrictLogging {
       .replaceAll(markerRegex, "") // replace song markers like [Verse], [Chorus 2], etc
       .replaceAll(" +", " ") // remove double spaces that may have been introduced
       .trim
-
 }
--- src/test/scala/service/request/genius/GeniusLyricsScraperTest.scala
+++ src/test/scala/service/request/genius/GeniusLyricsScraperTest.scala
@@ -1,17 +1,28 @@
 package service.request.genius
 
+import models.api.response.GeniusSong
 import testutils.UnitSpec
 
 class GeniusLyricsScraperTest extends UnitSpec {
-  val scraper = new GeniusLyricsScraper
-  val url = "https://genius.com/David-bowie-space-oddity-lyrics"
+  private val scraper = new GeniusLyricsScraper
+  private val song1 = GeniusSong(1, "space oddity", "https://genius.com/David-bowie-space-oddity-lyrics")
+  private val lyrics1 = "This is Ground Control to Major Tom"
+  private val song2 = GeniusSong(2, "come together", "https://genius.com/The-beatles-come-together-lyrics")
+  private val lyrics2 = "Got to be good-lookin' 'cause he's so hard to see"
 
   "stripSongMarkers" should "remove song markers from lyrics text" in {
     val lyrics = "[Verse1] verse [Chorus] chorus chorus [Verse 2] hello [Bridge] bridge [Outro]"
     scraper.cleanLyrics(lyrics) shouldEqual "verse chorus chorus hello bridge"
   }
 
+  "scrapeLyrics" should "corresponding lyrics futures for each song" in {
+    val songs = Seq(song1, song2)
+    val results = scraper.scrapeLyrics(songs)
+    whenReady(results.head._2)(_.contains(lyrics1) should be (true))
+    whenReady(results(1)._2)(_.contains(lyrics2) should be (true))
+  }
+
   "scrapeLyrics" should "return lyrics for the given song url" in {
-    scraper.scrapeLyrics(url).contains("This is Ground Control to Major Tom") should be (true)
+    scraper.scrapeLyrics(song1.url).contains(lyrics1) should be (true)
   }
 }
